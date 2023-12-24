2d2146290400a1d4cfc4221a5de5e9756d557359
ilyasdev3 <ilyassanjrani708@gmail.com>
2023-12-24 19:47:53 +0500

    add main file react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,15 +7,21 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router } from "react-router-dom";
 import Loader from "./components/reuseable/Loader.tsx";
+import { Providers } from "./redux-store/provider.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 library.add(fas);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Suspense fallback={<Loader />}>
-      <Router>
-        <App />
-      </Router>
-    </Suspense>
+    <Providers>
+      <Suspense fallback={<Loader />}>
+        <Router>
+          <ToastContainer />
+          <App />
+        </Router>
+      </Suspense>
+    </Providers>
   </React.StrictMode>
 );

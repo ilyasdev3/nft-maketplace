d7ff51da6386f513ca07d95274f562e0bf222343
ilyasdev3 <ilyassanjrani708@gmail.com>
2023-12-24 15:29:12 +0500

    Basic backend setup folder structure

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api", (req, res) => {
+  res.send({ express: "Hello From Express" });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port} !`));

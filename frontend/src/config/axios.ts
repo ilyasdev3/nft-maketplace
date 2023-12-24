683d5c0f901e24a4e09132a8922ca1ad530ba869
ilyasdev3 <ilyassanjrani708@gmail.com>
2023-12-24 19:47:25 +0500

    add axios config file

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,20 +1,11 @@
 import axios from "axios";
-import { HOTEL_BOOKING_TOKEN } from "../utils/constant";
 
 const axiosInstance = () => {
-  //getting token from local storage
-  const token = localStorage.getItem(HOTEL_BOOKING_TOKEN);
+  const token = localStorage.getItem("nfttoken");
 
   //setting the enviroment
-  let url: any = "http://localhost:8800";
-  // if (process.env.NEXT_PUBLIC_NODE_ENV === "local") {
-  //   url = process.env.NEXT_PUBLIC_BACKEND_LOCAL_BASE_URL;
-  // } else if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
-  //   url = process.env.NEXT_PUBLIC_BACKEND_DEVELOPMENT_BASE_URL;
-  // } else if (process.env.NEXT_PUBLIC_NODE_ENV === "staging") {
-  //   url = process.env.NEXT_PUBLIC_BACKEND_STAGING_BASE_URL;
-  // } else if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
-  //   url = process.env.NEXT_PUBLIC_BACKEND_PRODUCTION_BASE_URL;
+  let url: any = "http://localhost:5000";
+
   // }
   const enviroment = `${url}/api`;
   return axios.create({

5b3c2f74d4df00e0075d092496ed06ba36be825c
ilyasdev3 <ilyassanjrani708@gmail.com>
2023-12-24 15:55:28 +0500

    Add cors and setup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/api", (req, res) => {
-  res.send({ express: "Hello From Express" });
+  res.send({ express: "Hello From Nft marketplace" });
 });
 
 app.listen(port, () => console.log(`Server running on port ${port} !`));
